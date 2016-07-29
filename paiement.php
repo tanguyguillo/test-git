424b7b3643c439251d5178b9aef030c7835792f8
Tanguy Guillo <tanguy.guillo@gmail.com>
2016-07-29 15:39:14 +0200

    commit sur branche paiement

---
+++ paiement.php
@@ -0,0 +1,43 @@
+[33mcommit 29850354ca8397599cbc545272c98abbe45719a8[m
+Author: Tanguy Guillo <tanguy.guillo@gmail.com>
+Date:   Fri Jul 29 15:07:43 2016 +0200
+
+    test
+
+[33mcommit 95010d87e4e068769a19009f2de4d4757999b352[m
+Author: formation <formation>
+Date:   Fri Jul 29 14:51:50 2016 +0200
+
+    message aprés reunification
+
+[33mcommit bb46825d20904f509d27d285f40357e88f85985e[m
+Author: formation <formation>
+Date:   Fri Jul 29 14:41:55 2016 +0200
+
+    ajout et modif panier
+
+[33mcommit f114ec730161d14ece7e2146f15b8e4f7beaae52[m
+Author: formation <formation>
+Date:   Fri Jul 29 14:35:39 2016 +0200
+
+    ajout panier
+
+[33mcommit e4d821b06b621e38b7c1819ec726b26f53e69ba7[m
+Author: formation <formation>
+Date:   Fri Jul 29 14:20:11 2016 +0200
+
+    barre de nav avec CSS
+
+[33mcommit 14ef639bb20118c3f71eca9e1ba5b158012184c0[m
+Author: formation <formation>
+Date:   Fri Jul 29 13:18:56 2016 +0200
+
+    test 2
+
+[33mcommit 2a7a9644561680ccb5e2d04ec3c9336a1cd1acfb[m
+Author: formation <formation>
+Date:   Fri Jul 29 13:07:41 2016 +0200
+
+    creation index
+    
+    et du css
